3c7d44e4054cb0825065ef17cc89260474a24d93
M-Eleoterio <m.eleoteriovieira@gmail.com>
2024-08-16 02:14:10 -0300

    profile(feat): termina o front dos perfis

--- frontend/socialmedia/src/Components/CpProfile/cpProfile.jsx
+++ frontend/socialmedia/src/Components/CpProfile/cpProfile.jsx
@@ -1,22 +1,57 @@
+import { useParams } from 'react-router-dom';
 import './cpProfile.css';
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 export default function CpProfile() {
+    const { id } = useParams();
+    const [user, setUser] = useState();
 
+    function getProfile() {
+        axios.get(`http://localhost:8000/api/users/${id}`, {
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem('token')}`
+            }
+        })
+            .then(res => {
+                console.log(res.data);
+                setUser(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
 
+    useEffect(() => {
+        getProfile();
+    }, [])
 
     return (
         <div id="profile-container">
             <div id="profile-top">
                 <section id="profile-section-icon">
+                    <img src="https://placehold.jp/400x400.png" alt="" id="profile-section-icon-img" />
                 </section>
                 <section id="profile-section-info">
-                    <span id="profile-section-info-name"></span>
-                    <span id="profile-section-info-nick"></span>
-                    <span id="profile-section-info-desc"></span>
+                    <h3 className='profile-section-info-text' id="profile-section-info-name">{user.name}</h3>
+                    <p className='profile-section-info-text' id="profile-section-info-email">{user.email}</p>
+                    <p className='profile-section-info-text' id="profile-section-info-desc">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus maxime, aperiam consectetur quod ex adipisci voluptates nemo dicta laudantium, provident cumque perspiciatis odit sed, molestias blanditiis error. Repellendus, voluptatum provident.
+                    </p>
+                    <div id="profile-section-info-btns">
+                        <button className="profile-section-info-btn">Follow</button>
+                        <button className="profile-section-info-btn">Send Message</button>
+                    </div>
                 </section>
             </div>
-            <div id="porfile-posts"></div>
+            <hr />
+            <div id="profile-posts">
+                <div className="profile-post"></div>
+                <div className="profile-post"></div>
+                <div className="profile-post"></div>
+                <div className="profile-post"></div>
+                <div className="profile-post"></div>
+            </div>
         </div>
     )
 }
--- frontend/socialmedia/src/Pages/profile/pgProfile.jsx
+++ frontend/socialmedia/src/Pages/profile/pgProfile.jsx
@@ -1,10 +1,12 @@
 import './pgProfile.css';
 import CpSidebar from '../../Components/cpSidebar/cpSidebar';
+import CpProfile from '../../Components/CpProfile/cpProfile';
 
 export default function PgProfile() {
     return (
         <div id="profile">
             <CpSidebar />
+            <CpProfile />
         </div>
     )
 }
